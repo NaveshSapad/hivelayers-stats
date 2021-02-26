fc97677ba08f8f8afc33ea7795c05e5a84cce2f7
NaveshSapad <navesh.sapad@gmail.com>
2021-02-26 20:46:14 +0530

    '26thfeb'

--- hivelayers-stats.py
+++ hivelayers-stats.py
@@ -421,9 +421,9 @@ def hivecommunity():
 
     d = st.date_input(
          "Choose the date to display the data",
-         datetime.date(2021, 2, 24),
+         datetime.date(2021, 2, 25),
          min_value=datetime.date(2021, 2, 15),
-         max_value=datetime.date(2021, 2, 24))
+         max_value=datetime.date(2021, 2, 25))
 
     st.write('Selected Date:', str(d))
 
